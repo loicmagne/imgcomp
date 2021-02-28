5c8f74b73b162f3dcfdabba7daa5fa13f82d8f61
Loïc Magne <loic.magne@outlook.com>
2021-02-28 02:46:46 +0100

    Added knn comparator

--- src.py
+++ src.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import math
 from PIL import Image
+from sklearn.neighbors import KNeighborsRegressor
 
 class Error(Exception):
     pass
@@ -56,11 +57,11 @@ class ImageComparator():
         else:
             raise Error('Images not similar enough')
         
-    def probabilityMap(self,kp1,kp2,img_size,tile_size,diff_threshold=5):
+    def tilingComparison(self,kp1,kp2,img_size,tile_size,diff_threshold,plot_data=False):
         '''
-        Output a probability grid where each cell gives the probability
-        that there is a difference between the two images in the given
-        cell
+        Use a tiling method to compare keypoints from both image: we 
+        divide the compared image into cells and count the ratio of
+        keypoints  in each cells
         
         Parameters
         ----------
@@ -80,15 +81,62 @@ class ImageComparator():
             t2[int(k.pt[1]//tile_size),int(k.pt[0]/tile_size)] += 1
 
         t = (t2-t1)
-        t[t<=5] = 0
+        t[t<=np.percentile(t,95)] = 0
         t /= (t1+t2+10e-3)
         t = cv.resize(t,img_size)[:h,:w]
         
-        # bins = np.linspace(t.min(),t.max(), 50)
-        # plt.xlim([t.min(), t.max()])
-        # plt.hist(t.flatten(), bins=bins)
-        # plt.show()
-        return np.uint8(255 * t)
+        if plot_data:
+            bins = np.linspace(t.min()-0.1,t.max()+0.1, 50)
+            plt.xlim([t.min(), t.max()])
+            plt.hist(t.flatten(), bins=bins)
+            plt.show()
+        
+        return t
+            
+    def knnComparison(self,kp1,kp2,img_size,k):
+        '''
+        Use a knn method to compare keypoints from both image: each
+        pixel probability of being different is the ratio of nearest
+        keypoints in kp2 among all neighbors
+        
+        Parameters
+        ----------
+        kp1, kp2 : cv.KeyPoint list
+        img_size : (int,int)
+        k : int
+            knn hyperparameter
+        '''
+        w,h = img_size
+        X = [kp.pt for kp in kp1+kp2]
+        y = np.concatenate((np.zeros(len(kp1)),np.ones(len(kp2))))
+        knn = KNeighborsRegressor(n_neighbors=k,weights='distance')
+        knn.fit(X, y)
+        
+        Xs = np.arange(w)
+        Ys = np.arange(h)
+        xx, yy = np.meshgrid(Xs,Ys)
+        XX = np.stack((np.ravel(xx),np.ravel(yy)),axis=-1)
+        Z = np.reshape(knn.predict(XX),(h,w))
+        return np.square(Z)
+        
+        
+    def probabilityMap(self,kp1,kp2,img_size,tile_size=100,diff_threshold=5,k=10):
+        '''
+        Output a probability grid where each cell gives the probability
+        that there is a difference between the two images in the given
+        cell
+        
+        Parameters
+        ----------
+        kp1, kp2 : cv.KeyPoint list
+        img_size : (int,int)
+        tile_size : int
+        diff_threshold : int
+        k : int
+        '''
+        t1 = self.knnComparison(kp1,kp2,img_size,k)
+        t2 = self.tilingComparison(kp1,kp2,img_size,tile_size,diff_threshold)
+        return np.uint8(255*t1*t2)
         
     def compare(self,src,trgt,dest):
         '''
@@ -137,18 +185,27 @@ class ImageComparator():
         kp2_new = [cv.KeyPoint(pt[0],pt[1],5) for pt in kp2_new_pt]
         
         # get probability map
-        heatmap = self.probabilityMap(kp1,kp2_new,(w1,h1),100)
-        heatmap = cv.applyColorMap(heatmap, cv.COLORMAP_JET)
+        heatmap = self.probabilityMap(kp1,kp2_new,(w1,h1),50)
+        heatmap = cv.applyColorMap(heatmap, cv.COLORMAP_JET)        
         
         # combine heatmap with warped image
         display = cv.addWeighted(result,0.5,heatmap,0.3,0)
         
+        # plot keypoints
+        kp_res = cv.drawKeypoints(result, kp1, None, color=(0, 0, 255))
+        kp_res = cv.drawKeypoints(kp_res, kp2_new, None, color=(0, 255, 0))
+        
         # create a comparison image
-        final = np.hstack((src,result,display))
+        final1 = np.hstack((src,result))
+        final2 = np.hstack((kp_res,display))
+        final = np.vstack((final1,final2))
+        
         font = cv.FONT_HERSHEY_SIMPLEX
-        cv.putText(final,'reference',(0,25), font, 1,(0,255,0),2,cv.LINE_AA)
-        cv.putText(final,'target',(w1,25), font, 1,(0,255,0),2,cv.LINE_AA)
-        cv.putText(final,'comparison',(2*w1,25), font, 1,(0,255,0),2,cv.LINE_AA)
+        color = (255,255,0)
+        cv.putText(final,'reference',(0,25), font, 1,color,2,cv.LINE_AA)
+        cv.putText(final,'target',(w1,25), font, 1,color,2,cv.LINE_AA)
+        cv.putText(final,'keypoints',(0,h1+25), font, 1,color,2,cv.LINE_AA)
+        cv.putText(final,'comparison',(w1,h1+25), font, 1,color,2,cv.LINE_AA)
         
         # save the comparison
         cv.imwrite(dest,final)
